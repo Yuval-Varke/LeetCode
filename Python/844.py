9c26dd8e9ddf1bce134fdd206cc38502324f41cf
Yuval Varke <varke.vishu01@gmail.com>
2026-04-04 09:18:27 +0530

    844. Backspace String Compare

---
+++ Python/844.py
@@ -0,0 +1,68 @@
+# 844. Backspace String Compare
+
+# Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+# Note that after backspacing an empty text, the text will continue empty.
+
+ 
+
+# Example 1:
+
+# Input: s = "ab#c", t = "ad#c"
+# Output: true
+# Explanation: Both s and t become "ac".
+# Example 2:
+
+# Input: s = "ab##", t = "c#d#"
+# Output: true
+# Explanation: Both s and t become "".
+# Example 3:
+
+# Input: s = "a#c", t = "b"
+# Output: false
+# Explanation: s becomes "c" while t becomes "b".
+ 
+
+# Constraints:
+
+# 1 <= s.length, t.length <= 200
+# s and t only contain lowercase letters and '#' characters.
+
+
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        i, j = len(s) - 1, len(t) - 1
+        skip1, skip2 = 0, 0
+        
+        while i >= 0 or j >= 0:
+            while i >= 0:
+                if s[i] == '#':
+                    skip1 += 1
+                    i -= 1
+                elif skip1 > 0:
+                    skip1 -= 1
+                    i -= 1
+                else:
+                    break
+            
+            while j >= 0:
+                if t[j] == '#':
+                    skip2 += 1
+                    j -= 1
+                elif skip2 > 0:
+                    skip2 -= 1
+                    j -= 1
+                else:
+                    break
+            
+            if i >= 0 and j >= 0 and s[i] != t[j]:
+                return False
+            
+            if (i >= 0) != (j >= 0):
+                return False
+            
+            i -= 1
+            j -= 1
+            
+        return True
